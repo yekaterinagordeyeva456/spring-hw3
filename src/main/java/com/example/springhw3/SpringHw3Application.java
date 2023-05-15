2657b14c895edf6d80813ffc098b4b663e6c806e
yekaterinagordeyeva456 <yges1979@gmail.com>
2023-05-16 02:19:13 +0600

    initial commit

---
+++ src/main/java/com/example/springhw3/SpringHw3Application.java
@@ -0,0 +1,13 @@
+package com.example.springhw3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHw3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHw3Application.class, args);
+    }
+
+}
